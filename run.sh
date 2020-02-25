b5699dc1290b395345e7977ea990a568a43e5ebf
Yulin Shen <sylxjtu@outlook.com>
2020-02-25 16:35:08 +0800

    fix

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@ set -e
 hdfs dfs -mkdir -p project/raw_corpus
 hdfs dfs -mkdir -p project/corpus
 
-hdfs dfs -copyFromLocal word/word.txt project/word.txt
-hdfs dfs -copyFromLocal corpus/geo.txt project/raw_corpus/geo.txt
-hdfs dfs -copyFromLocal corpus/geo_processed.txt project/corpus/geo_processed.txt
+hdfs dfs -copyFromLocal -f word/word.txt project/word.txt
+hdfs dfs -copyFromLocal -f corpus/geo.txt project/raw_corpus/geo.txt
+hdfs dfs -copyFromLocal -f corpus/geo_processed.txt project/corpus/geo_processed.txt
 
-java -cp target/mp_project-1.0-SNAPSHOT.jar mpproject.ACAM config.json
+hadoop jar target/mp_project-1.0-SNAPSHOT.jar mpproject.ACAM config.json
 hadoop jar target/mp_project-1.0-SNAPSHOT.jar mpproject.Main config.json
